e71b97c08dcd33b70860b0eb55cebef82f60bcc5
Himanish Setia <hsetia94@gmail.com>
2018-08-27 18:54:53 +0530

    HTML support in email
    Signed-off-by: Himanish Setia <hsetia94@gmail.com>

--- server.js
+++ server.js
@@ -18,11 +18,12 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.get('/sendMail',function(req,res){
+  var tempData = '<!DOCTYPE html><html lang="en"><body><style>.outer{position: absolute;left: 50%;top: 50%;border: 5px solid #a35656;padding: 10px 10px 10px 10px;font-family: berlin sans fb;}   .header{    text-align: center;font-weight: bold;font-size: 2em;} </style><div class="outer"><div class="header">Profile</div><hr><table cellpadding="10"><tr><td>Name</td><td>'+req.query.name+'</td></tr><tr><td>Email</td><td>'+req.query.email+'</td></tr><tr><td>Occupation</td><td>'+req.query.occupation+'</td></tr><tr><td>Comment</td><td>'+req.query.comment+'</td></tr></table></div></body></html>'
     var mailOptions = {
         from: 'noneedtoreplyprofile@gmail.com',
         to: 'hsetia94@gmail.com',
         subject: 'Profile Feedback',
-        text: req.query.comment
+        html: tempData
       };
       
       transporter.sendMail(mailOptions, function(error, info){
